44c034482a2a42384829f5c8d4f1c8ff5fb64418
Bosko Derikonja <derikonja.bosko@gmail.com>
2023-06-07 13:14:27 +0200

    add test for palindrome, which failed

---
+++ WordLibTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using WordLib;
+
+namespace WordLibTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void IsPalindrome()
+    {
+
+        Worder worder = new Worder();
+
+        // create an instant
+        string Palindrome = "radar";
+        // run the method under test
+        bool resultat = worder.Ispalindrome();
+
+        //verify the result
+        Assert.IsTrue(resultat);
+
+
+    }
+}
+
---
+++ Worder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace WordLib
+{
+	public class Worder
+	{
+		public Worder()
+		{
+		}
+
+        public bool Ispalindrome()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
